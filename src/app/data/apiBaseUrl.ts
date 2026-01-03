1fd0e0e7259b5ccfa22974259c5d93963684c592
Andrey Khalepov <w54661c@gmail.com>
2026-01-03 14:02:12 +1000

    build api_url fix

--- src/app/data/apiBaseUrl.ts
+++ src/app/data/apiBaseUrl.ts
@@ -1,6 +1,8 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001";
 // Use localhost for both development and build time
 // In production runtime, this will be proxied/rewritten by the web server
-const host = typeof window === 'undefined'
-  ? 'http://localhost:5001'  // Server-side (including build time)
-  : '';  // Client-side (browser)
+const host =
+    typeof window === "undefined"
+        ? API_URL // Server-side (including build time)
+        : ""; // Client-side (browser)
 export const baseUrl = `${host}/api/v1/`;
